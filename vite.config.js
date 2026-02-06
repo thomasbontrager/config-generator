dcbb1c72a8b2ae95af576e39fcd878b0a7b4cf84
Thomas Bontrager <thomas.bontrager.dev@gmail.com>
2026-02-06 12:10:02 -0800

    Update base path for custom domain configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/config-generator/", // MUST match repo name
+  base: "/", // correct for custom domain
 });
